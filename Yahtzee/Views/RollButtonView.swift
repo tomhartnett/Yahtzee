da6a9c283e262eaf6b3a8ebed7206a605f40fc99
Tom Hartnett <tomhartnett@gmail.com>
2025-05-24 14:35:21 -0500

    Added bounce animation to roll button.

--- Yahtzee/Views/RollButtonView.swift
+++ Yahtzee/Views/RollButtonView.swift
@@ -10,9 +10,20 @@ import YahtzeeKit
 
 struct RollButtonView: View {
     @Binding var game: Game
+    @State private var scale = 1.0
 
     var body: some View {
         Button(action: {
+            withAnimation(.bouncy) {
+                scale *= 1.05
+            }
+
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                withAnimation(.bouncy) {
+                    scale = 1.0
+                }
+            }
+
             game.diceCup.roll()
             game.opponentLastTurn = nil
             if let values = game.diceCup.values {
@@ -28,13 +39,9 @@ struct RollButtonView: View {
                     .frame(width: 20)
                     .foregroundStyle(game.diceCup.remainingRolls > 0 ? .yellow : .gray)
 
-
-
                 Circle()
                     .frame(width: 20)
                     .foregroundStyle(game.diceCup.remainingRolls > 1 ? .yellow : .gray)
-
-
 
                 Circle()
                     .frame(width: 20)
@@ -44,6 +51,8 @@ struct RollButtonView: View {
             .frame(maxWidth: .infinity, minHeight: 40)
         }
         .buttonStyle(BorderedProminentButtonStyle())
+        .scaleEffect(scale)
+        .animation(.spring(duration: 0.25, bounce: 0.75), value: scale)
         .disabled(game.diceCup.remainingRolls <= 0 || game.playerScorecard.isFull)
     }
 }
