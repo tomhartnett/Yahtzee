50eafce6b63acfecaff422767af6b61c9a158774
Tom Hartnett <tomhartnett@gmail.com>
2025-08-16 13:15:01 -0500

    Added YahtzeeKit test plan and a couple tests.

---
+++ YahtzeeKit/Tests/YahtzeeKitTests/Array+ExtensionsTests.swift
@@ -0,0 +1,37 @@
+//
+//  Array+ExtensionsTests.swift
+//  YahtzeeKit
+//
+//  Created by Tom Hartnett on 8/16/25.
+//
+
+import Testing
+@testable import YahtzeeKit
+
+struct ArrayExtensionsTests {
+    @Test func safeSubscript() {
+        // Given
+        let array = ["a", "b", "c"]
+
+        // Then
+        #expect(array[safe: 0] == "a")
+        #expect(array[safe: 1] == "b")
+        #expect(array[safe: 2] == "c")
+        #expect(array[safe: 3] == nil)
+    }
+
+    @Test func randomElement() {
+        // Given
+        var array = ["a", "b", "c"]
+
+        // Then
+        #expect(array.randomElement(where: { $0 == "b" }) == "b")
+        #expect(array.randomElement(where: { $0 == "not found" }) == nil)
+
+        // When
+        array.removeAll()
+
+        // Then
+        #expect(array.randomElement(where: { $0 == "b" }) == nil)
+    }
+}
